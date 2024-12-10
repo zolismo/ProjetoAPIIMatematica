10f6bcacaef65177649e127248b85cf451134838
zolismo <140122910+zolismo@users.noreply.github.com>
2024-12-10 01:04:30 -0300

    .
    .

--- src/main/java/br/com/opusnet/projetoapiidoscrias/view/scene/GameWinScene.java
+++ src/main/java/br/com/opusnet/projetoapiidoscrias/view/scene/GameWinScene.java
@@ -2,6 +2,7 @@ package br.com.opusnet.projetoapiidoscrias.view.scene;
 
 import br.com.opusnet.projetoapiidoscrias.controlls.GameLoop;
 import br.com.opusnet.projetoapiidoscrias.controlls.screencontrol.GameWinController;
+import br.com.opusnet.projetoapiidoscrias.controlls.screencontrol.SceneOneController;
 import br.com.opusnet.projetoapiidoscrias.model.Controll;
 import br.com.opusnet.projetoapiidoscrias.model.util.Updatable;
 import javafx.animation.FadeTransition;
@@ -51,6 +52,7 @@ public class GameWinScene extends Scene implements Updatable {
     public void verifyQuit(){
         Platform.runLater(()->{
             if(controller.b_quit.isPressed()){
+                SceneOneController.movement = 1;
                 gameLoop.stop();
                 System.exit(0);
             }
